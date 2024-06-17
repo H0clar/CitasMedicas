9ca0f88c3c4c462dfb5846f32c81516556c84138
H0clar <juromero22@cftsa.cl>
2024-06-16 23:11:56 -0400

    correccion seeder, migrations medicos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            MedicoSeeder::class, 
             RolesTableSeeder::class,
             UserSeeder::class,
-            MedicoSeeder::class, 
+            
         ]);
     }
 }
